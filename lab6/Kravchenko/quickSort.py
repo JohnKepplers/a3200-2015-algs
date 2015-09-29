64641caa9d612703d74f1d46fba4edfbec821c36
Alexandr Kravchenko <sasha121196@mail.ru>
2015-09-30 01:56:35 +0300

    Update quickSort.py

--- lab6/Kravchenko/quickSort.py
+++ lab6/Kravchenko/quickSort.py
@@ -7,23 +7,39 @@ d = {}
 '''test = [int(0) for i in range(0, 10000)]
 for i in range(len(test)):
     test[i] = 2'''
-counter = 0
-for i in range(len(a)):
-    if d.get(a[i]) == None:
-        d.update({a[i]: 1})
-    else:
-        d[a[i]] += 1
-ar = [0] * len(d)
-for key in d.keys():
-    ar[counter] = key
-    counter += 1
+
+
+def crutch(a):
+    counter = 0
+    for i in range(len(a)):
+        if d.get(a[i]) == None:
+            d.update({a[i]: 1})
+        else:
+            d[a[i]] += 1
+    ar = [0] * len(d)
+    for key in d.keys():
+        ar[counter] = key
+        counter += 1
+    if counter != 0:
+        quick_sort(ar, 0, len(ar) - 1)
+        counter = 0
 
 
 def quick_sort(a, p, r):
+    global h
+
+    if p <= r:
+        h = 0
     if p < r:
+        h = 0
         q = randomised_partition(a, p, r)
         quick_sort(a, p, q - 1)
         quick_sort(a, q + 1, r)
+    if h == 0:
+        another_crutch(a)
+        h = 1
+
+
 
 
 def randomised_partition(a, p, r):
@@ -39,16 +55,25 @@ def randomised_partition(a, p, r):
     return i + 1
 
 
-array = [0] * len(a)
-j = 0
-for key, value in d.items():
-    array[j] = key
-    j += 1
-    for i in range(value - 1):
-        array[j] = key
+def another_crutch(a):
+    counter = 0
+    print(counter)
+    for key, value in d.items():
+        if d[key] > 1:
+            counter += d[key] - 1
+
+    array = [0 for y in range(len(a) + counter)]
+    j = 0
+    l = 0
+    for key in d.keys():
+        array[j] = a[l]
         j += 1
+        l += 1
+        for i in range(d[key] - 1):
+            array[j] = key
+            j += 1
+    return print(array)
 
-quick_sort(ar, 0, len(ar) - 1)
-stdout.write(str(a))
 
 
+crutch(a)
