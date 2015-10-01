f14f4b9e6a3059289a71eb4732825602fcc55bf1
Alexandr Kravchenko <sasha121196@mail.ru>
2015-10-01 22:42:57 +0300

    Update lists.py

--- lab7/Kravchenko/lists.py
+++ lab7/Kravchenko/lists.py
@@ -7,18 +7,18 @@ def positive_and_negative_random_list(length):
 
 
 def positive_random_list(length):
-    a = [randint(0, 1000000) for i in range(length)]
+    a = [randint(0, 10000) for i in range(length)]
     return a
 
 
 def strange_sorted_list(length):
     j = 0
     a = [0 for i in range(length)]
-    a[0] = randint(0, 1000000)
+    a[0] = randint(0, 10000)
     l = 1
     while l < length:
         if j % 8 <= 3:
-            p = randint(0, 1000000)
+            p = randint(0, 10000)
             if p > a[l - 1]:
                 a[l] = p
                 j += 1
@@ -26,7 +26,7 @@ def strange_sorted_list(length):
             else:
                 continue
         if j % 8 > 3:
-            p = randint(0, 1000000)
+            p = randint(0, 10000)
             if p < a[l - 1]:
                 a[l] = p
                 j += 1
