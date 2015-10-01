7610558341eec6b60dd305c329b1ef7acca31e3b
Alexandr Kravchenko <sasha121196@mail.ru>
2015-10-01 21:54:23 +0300

    Create lists.py

---
+++ lab7/Kravchenko/lists.py
@@ -0,0 +1,48 @@
+from random import randint
+
+
+def positive_and_negative_random_list(length):
+    a = [randint(-1000000, 1000000) for i in range(length)]
+    return a
+
+
+def positive_random_list(length):
+    a = [randint(0, 1000000) for i in range(length)]
+    return a
+
+
+def strange_sorted_list(length):
+    j = 0
+    a = [0 for i in range(length)]
+    a[0] = randint(0, 1000000)
+    l = 1
+    while l < length:
+        if j % 8 <= 3:
+            p = randint(0, 1000000)
+            if p > a[l - 1]:
+                a[l] = p
+                j += 1
+                l += 1
+            else:
+                continue
+        if j % 8 > 3:
+            p = randint(0, 1000000)
+            if p < a[l - 1]:
+                a[l] = p
+                j += 1
+                l += 1
+            else:
+                continue
+
+    return a
+
+
+def normal_sort_list(length):
+    a = positive_random_list(length)
+    a.sort()
+    return a
+
+
+def abnormal_sort_list(length):
+    a = normal_sort_list(length)
+    return a[::-1]
