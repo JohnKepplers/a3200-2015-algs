361e53c03e09f2d49de5a99cbeac5f57ab3d0788
Alexandr Kravchenko <sasha121196@mail.ru>
2015-10-19 17:55:18 +0300

    Update quick_sort.py

--- lab5/Kravchenko/quick_sort.py
+++ lab5/Kravchenko/quick_sort.py
@@ -1,62 +1,35 @@
-from sys import stdin, stdout, maxsize
 from random import randint
-
-d = {}
-
-first_line = stdin.readline()
-a = [int(j) for j in first_line.split()]
-
-
-def crutch(a):
-    ar = []
-    for i in range(len(a)):
-        if d.get(a[i]) == None:
-            d.update({a[i]: 1})
-            ar.append(a[i])
-        else:
-            d[a[i]] += 1
-    print(ar)
-    quick_sort(ar, 0, len(ar) - 1)
-    print(another_crutch(ar))
+from sys import stdin, stdout
 
 
 def quick_sort(a, p, r):
     if p < r:
-        q = randomised_partition(a, p, r)
-        quick_sort(a, p, q - 1)
-        quick_sort(a, q + 1, r)
+        first, last = randomised_partition(a, p, r)
+        quick_sort(a, p, first)
+        quick_sort(a, last + 1, r)
     return a
 
 
 def randomised_partition(a, p, r):
     rand = randint(p, r)
-    a[rand], a[r] = a[r], a[rand]
-    x = a[r]
-    i = p - 1
-    for j in range(p, r):
-        if a[j] < x:
-            i += 1
-            a[i], a[j] = a[j], a[i]
-    a[i + 1], a[r] = a[r], a[i + 1]
-    return i + 1
+    pivot = a[rand]
+    counter = p
+    first = p
+    last = r
+    while counter <= last:
+        if a[counter] > pivot:
+            a[counter], a[last] = a[last], a[counter]
+            last -= 1
+
+        elif a[counter] < pivot:
+            a[counter], a[first] = a[first], a[counter]
+            first += 1
+            counter += 1
+        else:
+            counter += 1
+    return first, last
 
 
-def another_crutch(a):
-    counter = 0
-    for i in range(len(a)):
-        if d[a[i]] > 1:
-            counter += d[a[i]] - 1
-
-    array = []
-    print(a)
-    for l in range(len(a)):
-        array.append(a[l])
-        while d[a[l]] > 1:
-            array.append(a[l])
-            d[a[l]] -= 1
-
-    return array
-
-
-r = [8, 3, 1, 3, 2, 3, 3, 2, 1]
-crutch(a)
+array = [int(i) for i in stdin.readline().split()]
+quick_sort(array, 0, len(array) - 1)
+stdout.write(str(array))
