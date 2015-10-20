d315c797fce7338b3713e52e8f36048f913af669
Alexandr Kravchenko <sasha121196@mail.ru>
2015-10-21 00:03:23 +0300

    Create test_for_lab9.py

---
+++ lab9/Kravchenko/test_for_lab9.py
@@ -0,0 +1,40 @@
+import unittest
+import lab8
+
+
+class TestSorting(unittest.TestCase):
+    def test_one(self):
+        my_heap = lab8.MinHeap()
+        k = 0
+        a = [-5, -3, -6, 8, 9, 5]
+        lab8.test(k, a, my_heap)
+        res = my_heap.print_array()
+        expected = []
+        self.assertEqual(expected, res)
+
+    def test_two(self):
+        my_heap = lab8.MinHeap()
+        k = 1
+        a = [-5, -3, -6, 8, 9, 5]
+        lab8.test(k, a, my_heap)
+        res = my_heap.print_array()
+        expected = [9]
+        self.assertEqual(expected, res)
+
+    def test_three(self):
+        my_heap = lab8.MinHeap()
+        k = 6
+        a = [-5, -3, -6, 8, 9, 5]
+        lab8.test(k, a, my_heap)
+        res = my_heap.print_array()
+        expected = [-6, -3, -5, 8, 9, 5]
+        self.assertEqual(expected, res)
+
+    def test_four(self):
+        my_heap = lab8.MinHeap()
+        k = 2
+        a = [2, 7, 4, 2]
+        lab8.test(k, a, my_heap)
+        res = my_heap.print_array()
+        expected = [4, 7]
+        self.assertEqual(expected, res)
