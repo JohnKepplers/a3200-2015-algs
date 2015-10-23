b559ec3f038c89749eb94ff14e5eecb80791dfe3
Alexander <Dongodfather@mail.ru>
2015-10-23 19:30:15 +0300

    lab9 part 2 completed

--- lab9/Sadovnikov/tests4lab9.py
+++ lab9/Sadovnikov/tests4lab9.py
@@ -1,13 +1,14 @@
-from random import randint
+from random import randint, random
 
 __author__ = 'alexkane'
 
 import unittest
 import lab9
+import lab9part2
 
 class TestSorting(unittest.TestCase):
 
-    def test_trivial(self):
+    def part1_test_trivial(self):
         queue = lab9.HeapPriorityQueue()
         queue.set_special_k(1)
         queue.insert(2)
@@ -16,7 +17,7 @@ class TestSorting(unittest.TestCase):
         res = queue.get_array()[0]
         self.assertEqual(expected, res)
 
-    def test_hardcore(self):
+    def part1_test_hardcore(self):
         queue = lab9.HeapPriorityQueue()
         queue.set_special_k(10)
         for i in range(101):
@@ -26,4 +27,10 @@ class TestSorting(unittest.TestCase):
                 queue.insert(1111)
         expected = [1111, 1111, 1111, 1111, 1111, 1111, 1111, 1111, 1111, 1111]
         res = queue.get_array()
+        self.assertEqual(expected, res)
+
+    def part2_test_trivial(self):
+        array = [1, 0, 8, 2, 1]
+        res = lab9part2.quick_search(array, 0, 5)
+        expected = [0, 1, 1, 2, 8]
         self.assertEqual(expected, res)
