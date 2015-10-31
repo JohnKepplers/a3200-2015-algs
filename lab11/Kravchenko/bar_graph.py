c26a1ec20bf1648236eb0c1b9e9efbc568903dad
Alexandr Kravchenko <sasha121196@mail.ru>
2015-10-31 20:21:49 +0300

    Update bar_graph.py

--- lab11/Kravchenko/bar_graph.py
+++ lab11/Kravchenko/bar_graph.py
@@ -4,8 +4,19 @@ from sys import stdin, stdout
 def search(ar):
     max_s = 0
     s = 0
-    maximum = ar[0]
+    if len(ar) != 0:
+        maximum = ar[0]
     for i in range(1, len(ar)):
+        if ar[i] < maximum:
+            s += maximum - ar[i]
+        else:
+            if s > max_s:
+                max_s = s
+            s = 0
+            maximum = ar[i]
+    s = 0
+    maximum = ar[len(ar) - 1]
+    for i in range(len(ar) - 2, -1, -1):
         if ar[i] < maximum:
             s += maximum - ar[i]
         else:
