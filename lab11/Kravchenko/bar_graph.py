69e3946643865df4068ecbfeae0a307cb7e6d7e0
Alexandr Kravchenko <sasha121196@mail.ru>
2015-10-31 19:31:11 +0300

    Update bar_graph.py

--- lab11/Kravchenko/bar_graph.py
+++ lab11/Kravchenko/bar_graph.py
@@ -1,24 +1,23 @@
 from sys import stdin, stdout
 
+testing_array = [int(i) for i in stdin.readline().split()]
+
 
 def search(ar):
-    a = []
     max_s = 0
     s = 0
-    a += [ar[0]]
+    maximum = ar[0]
     for i in range(1, len(ar)):
-        if ar[i] < a[0]:
-            a += [ar[i]]
+        if ar[i] < maximum:
+            s += maximum - ar[i]
         else:
-            for j in range(1, len(a)):
-                s += a[0] - a[j]
-            a = [ar[i]]
             if s > max_s:
                 max_s = s
             s = 0
+            maximum = ar[i]
     return max_s
 
 
 if __name__ == '__main__':
-    testing_array = [int(i) for i in stdin.readline().split()]
     stdout.write(str(search(testing_array)))
+
