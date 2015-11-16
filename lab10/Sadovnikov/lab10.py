3770ab62fa46b51134c8a7c7e75a05752b08c795
Alexander <Dongodfather@mail.ru>
2015-11-16 20:41:59 +0300

    Corrections in lab10's algorithm

--- lab10/Sadovnikov/lab10.py
+++ lab10/Sadovnikov/lab10.py
@@ -10,29 +10,32 @@ if __name__ == "__main__":
 
 def execute_search(array):
     n = len(array)
-    new_array = []
-    wicked_array = []
     counter = 0
     for i in range(n):
-        for j in range(i + 1, n):
-            x = math.sqrt(array[i] ** 2 + array[j] ** 2)
-            if x % 1 == 0:
-                new_array.append(int(x))
-                wicked_array.append(array[j])
-    wicked_array.sort()
-    for i in range(len(wicked_array)):
-        if wicked_array[i] != wicked_array[i - 1]:
-            for j in range(i + 1, len(wicked_array)):
-                if wicked_array[i] == wicked_array[j]:
-                    counter -= 1
-    for i in range(len(new_array)):
-        for j in range(n):
-            if new_array[i] == array[j]:
-                counter += 1
+        array[i] **= 2
+    array.sort()
+    for i in range(n - 1):
+        j = i
+        k = 0
+        while True:
+            if j == k or j < k:
                 break
+            if array[k] + array[j] == array[i]:
+                counter += 1
+                j -= 1
+                while array[j] == array[j + 1]:
+                    j -= 1
+                k += 1
+                while array[k - 1] == array[k]:
+                    k += 1
+            elif array[k] + array[j] > array[i]:
+                j -= 1
+            else:
+                k += 1
     return counter
 
-if __name__ == "__main__":
-    print execute_search(input)
 
-# 18 100 99 24 40 10 10 24 4 30 3 5 1 88 24 32 32 26
+if __name__ == "__main__":
+    print(execute_search(input))
+
+    # 18 100 99 24 40 10 10 24 4 30 3 5 1 88 24 32 32 26
