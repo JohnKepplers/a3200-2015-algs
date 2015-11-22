72ea1162c340adda5cd9ceeacf6f709d02e7c1dc
Alexandr Kravchenko <sasha121196@mail.ru>
2015-11-23 03:14:15 +0400

    Update lab17.py

--- lab17/Kravchenko/lab17.py
+++ lab17/Kravchenko/lab17.py
@@ -1,9 +1,13 @@
+from union_find import DisjointSetUnion
+
+
 class WeightedGraph:
     def __init__(self):
         self.a = [[]]
         self.tree = [[]]
         self.d = {}
         self.list = []
+        self.maximum = None
 
     def size_of_matrix(self):
         return len(self.a)
@@ -19,9 +23,14 @@ class WeightedGraph:
 
     def add_vertex(self, v):
         self.a[0] += [v]
-        for k in range(1, self.size_of_matrix()):
-            self.a[k] += [0]
+        for i in range(1, self.size_of_matrix()):
+            self.a[i] += [0]
         self.a += [[0] * self.size_of_matrix()]
+        if self.maximum is None:
+            self.maximum = v
+        else:
+            if self.maximum < v:
+                self.maximum = v
 
     def add_directed_link(self, v1, v2, weight):
         if self.a[self.a[0].index(v1) + 1][self.a[0].index(v2)] == 0:
@@ -45,38 +54,35 @@ class WeightedGraph:
         return array
 
     def min_tree(self):
+        n = self.maximum
+        n += 1
+        my_disjoint_set_union = DisjointSetUnion(len(self.a[0]))
         for j in range(len(self.a[0])):
+            my_disjoint_set_union.make_set(self.a[0][j])
             self.tree[0] += [self.a[0][j]]
             for k in range(1, self.size_of_tree()):
                 self.tree[k] += [0]
             self.tree += [[0] * self.size_of_tree()]
         self.list.sort()
-        print(self.list)
-        print(self.d)
-        for f in range(len(self.list)):
-            array = self.d[self.list[f]]
-            print(array)
+        for i in range(len(self.list)):
+            array = self.d[self.list[i]]
             for j in range(0, len(array), 2):
-                index_one = self.a[0].index(array[j]) + 1
-                index_two = self.a[0].index(array[j + 1])
-                index_three = self.a[0].index(array[j+1]) + 1
-                index_four = self.a[0].index(array[j])
-                print(self.a[index_one][index_two])
-                if self.tree[index_one][index_two] == 0:
-                    self.tree[index_one][index_two] = self.a[index_one][index_two]
-                    self.tree[index_three][index_four] = self.a[index_three][index_four]
-                    print(self.tree)
-
+                if my_disjoint_set_union.find(array[j]) != my_disjoint_set_union.find(array[j + 1]):
+                    index_one = self.a[0].index(array[j])
+                    index_two = self.a[0].index(array[j + 1])
+                    my_disjoint_set_union.unite(array[j], array[j + 1])
+                    self.tree[index_one + 1][index_two] = self.a[index_one + 1][index_two]
+                    self.tree[index_two + 1][index_one] = self.a[index_two + 1][index_one]
 
 
 if __name__ == '__main__':
     graph = WeightedGraph()
-    for i in range(1, 5):
-        graph.add_vertex(i)
-    graph.add_directed_link(1, 2, 4)
-    graph.add_directed_link(2, 3, 4)
-    graph.add_directed_link(1, 3, 2)
-    graph.add_directed_link(3, 4, 3)
+    for k in range(4):
+        graph.add_vertex(k)
+    graph.add_directed_link(0, 1, 4)
+    graph.add_directed_link(1, 2, 3)
+    graph.add_directed_link(2, 3, 1)
+    graph.add_directed_link(0, 3, 2)
     graph.min_tree()
     print(graph.print_matrix())
     print(graph.print_tree())
