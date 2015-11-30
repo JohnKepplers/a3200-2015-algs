623adeec9e0a0ef5142afed916b6b69b12736d5b
Alexandr Kravchenko <sasha121196@mail.ru>
2015-12-01 02:12:23 +0400

    Update lab18.py

--- lab18/Kravchenko/lab18.py
+++ lab18/Kravchenko/lab18.py
@@ -3,18 +3,22 @@ from sys import stdin, stdout
 
 def distance(a, b):
     n, m = min(len(a), len(b)), max(len(a), len(b))
-    if len(a) < len(b):
+    if len(a) <= len(b):
         current_row = range(n + 1)
         for i in range(1, m + 1):
             previous_row, current_row = current_row, [i] + [0] * n
             for j in range(1, n + 1):
-                add, delete, change, transposition = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[j - 1], \
-                                                     previous_row[j - 1]
+                add, delete, change, transposition = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[
+                    j - 1], max(previous_row[j] + 1, current_row[j - 1] + 1, previous_row[
+                    j - 1])
                 if a[j - 1] != b[i - 1]:
                     change += 1
-                if a[j - 2] != b[i - 2]:
+                if a[j - 1] == b[i - 2]:
+                    transposition = previous_row[j - 1]
+                if a[j - 2] != b[i - 1]:
                     transposition += 1
                 current_row[j] = min(add, delete, change, transposition)
+            print(current_row)
 
         return current_row[n]
     else:
@@ -22,13 +26,17 @@ def distance(a, b):
         for i in range(1, m + 1):
             previous_row, current_row = current_row, [i] + [0] * n
             for j in range(1, n + 1):
-                add, delete, change, transposition = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[j - 1], \
-                                                     previous_row[j - 1]
+                add, delete, change, transposition = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[
+                    j - 1], max(previous_row[j] + 1, current_row[j - 1] + 1, previous_row[
+                    j - 1])
                 if b[j - 1] != a[i - 1]:
                     change += 1
-                if b[j - 2] != a[i - 2]:
+                if b[j - 1] == a[i - 2]:
+                    transposition = previous_row[j - 1]
+                if b[j - 2] != a[i - 1]:
                     transposition += 1
                 current_row[j] = min(add, delete, change, transposition)
+            print(current_row)
 
         return current_row[n]
 
@@ -37,3 +45,4 @@ if __name__ == '__main__':
     a = stdin.readline()
     b = stdin.readline()
     stdout.write(str((distance(a, b))))
+
