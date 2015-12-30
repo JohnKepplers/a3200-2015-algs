aa13bfae388af2640c92a06fd8f956af71728350
Alexandr Kravchenko <sasha121196@mail.ru>
2015-12-31 01:31:21 +0400

    Update lab16.py

--- lab16/Kravchenko/lab16.py
+++ lab16/Kravchenko/lab16.py
@@ -1,32 +1,32 @@
-from sys import maxsize as vasiliy_pisar
+from sys import maxsize, stdin, stdout
 import heapq
 
 
 class WeightedGraph:
     def __init__(self):
-        self.a = [[]]
+        self.graph = [[]]
         self.list = []
         self.d = {}
 
     def size_of_matrix(self):
-        return len(self.a)
+        return len(self.graph)
 
     def print_matrix(self):
-        return self.a
+        return self.graph
 
     def size_of_list(self):
         return len(self.list)
 
     def add_vertex(self, v):
-        self.a[0] += [v]
-        self.d.update({v: [vasiliy_pisar, None]})
+        self.graph[0] += [v]
+        self.d.update({v: [maxsize, None]})
         for i in range(1, self.size_of_matrix()):
-            self.a[i] += [0]
-        self.a += [[0] * self.size_of_matrix()]
+            self.graph[i] += [0]
+        self.graph += [[0] * self.size_of_matrix()]
 
     def add_directed_link(self, v1, v2, weight):
-        if self.a[self.a[0].index(v1) + 1][self.a[0].index(v2)] == 0:
-            self.a[self.a[0].index(v1) + 1][self.a[0].index(v2)] = weight
+        if self.graph[self.graph[0].index(v1) + 1][self.graph[0].index(v2)] == 0:
+            self.graph[self.graph[0].index(v1) + 1][self.graph[0].index(v2)] = weight
             self.d.update({v2: [weight, v1]})
 
         else:
@@ -39,12 +39,12 @@ class WeightedGraph:
 
     def initialize_single_source(self, s):
         for key, value in self.d.items():
-            self.d[key][0] = vasiliy_pisar
+            self.d[key][0] = maxsize
             self.d[key][1] = None
         self.d[s][0] = 0
 
-    def dijkstra(self, s):
-        self.initialize_single_source(s)
+    def paths(self, w):
+        self.initialize_single_source(w)
         q = []
         for key in self.d.keys():
             q += [key]
@@ -52,9 +52,20 @@ class WeightedGraph:
             u = heapq.heappop(q)
             self.list.append(u)
             for key in self.d.keys():
-                if self.a[self.a[0].index(u) + 1][self.a[0].index(key)] != 0:
-                    self.relax(u, key, self.a[self.a[0].index(u) + 1][self.a[0].index(key)])
+                if self.graph[self.graph[0].index(u) + 1][self.graph[0].index(key)] != 0:
+                    self.relax(u, key, self.graph[self.graph[0].index(u) + 1][self.graph[0].index(key)])
         array = []
         for key in self.d.keys():
             array += [self.d[key][0]]
         return array
+if __name__ == '__main__':
+    graph = WeightedGraph()
+    graph.add_vertex(1)
+    graph.add_vertex(9)
+    graph.add_vertex(4)
+    graph.add_vertex(5)
+    graph.add_directed_link(1, 5, 3)
+    graph.add_directed_link(5, 9, 7)
+    graph.add_directed_link(1, 9, 11)
+    graph.add_directed_link(9, 1, 1941)
+    print(graph.paths(1))
